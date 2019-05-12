e0ce484aede568ae92ced305291095bf6ba1942e
mntky <djminatoo@gmail.com>
2019-05-13 00:04:51 +0900

    [add]slack webhook POSTdata and official URL

---
+++ slackapi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+curl -d '{"username":"eliot", "text":"hey"}' https://hooks.slack.com/services/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
+
+#write type JSON
+#"username": "name"
+#"text": "test"
+
+#https://api.slack.com/incoming-webhooks
